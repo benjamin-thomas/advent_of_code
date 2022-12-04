5787ea9ed634dcf512a281b78ceab12896eb2b3a
Benjamin <benjamin.guy.thomas@gmail.com>
2022-12-04 17:58:31 +0100

    All tests setup

--- 2022/fsharp/Day01.fs
+++ 2022/fsharp/Day01.fs
@@ -1,3 +1,17 @@
 module Day01
 
-let greet = "hello day 1!"
+/// Greets with style (and documentation)!
+let greet = "Hello, day 1!"
+
+open Expecto
+
+let tests =
+    testList
+        "Day 01"
+        [
+            test "Can greet" { Expect.equal greet "Hello, day 1!" "Greeting failed, somehow!" }
+            test "Can do math" {
+                Expect.equal 2 (1 + 1) "wat?"
+                Expect.equal 4 (2 + 2) "wat??"
+            }
+        ]
--- 2022/fsharp/Program.fs
+++ 2022/fsharp/Program.fs
@@ -1,1 +1,13 @@
-﻿printfn "%s" Day01.greet
+﻿open Expecto
+
+(*
+  dotnet watch run
+*)
+
+[<EntryPoint>]
+let main args =
+  printfn "Running tests..."
+  runTestsWithCLIArgs [] args Day01.tests |> ignore
+  printfn $"%s{Day01.greet}"
+  0
+  
