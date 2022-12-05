cecb927c05504842d5487434e7de757d7378df52
Benjamin <benjamin.guy.thomas@gmail.com>
2022-12-05 22:37:04 +0100

    Day1: add rust solution

--- 2022/rust/src/lib.rs
+++ 2022/rust/src/lib.rs
@@ -1,16 +1,21 @@
-pub fn greet() -> String {
-    String::from("hello from lib!")
+pub fn from_input(input: &str) -> Vec<u32> {
+    return input
+        .split("\n\n")
+        .map(|x| x.lines().map(|x| x.parse::<u32>().unwrap()).sum())
+        .collect();
 }
 
-/// Adds in a smart way!
-///
-/// Examples:
-/// ```
-/// assert_eq!(aoc::add(1, 1), 2);
-/// assert_eq!(aoc::add(1, 2), 3);
-/// ```
-pub fn add(a: usize, b: usize) -> usize {
-    a + b
+pub fn most_calories(calories: Vec<u32>) -> u32 {
+    let mut cal = calories.clone();
+    cal.sort();
+    return cal.last().unwrap().clone();
+}
+
+pub fn top3_calories(calories: Vec<u32>) -> u32 {
+    let mut res = calories.clone();
+    res.sort_by(|a, b| b.cmp(a));
+    res = res[0..3].into();
+    res.iter().sum()
 }
 
 #[cfg(test)]
@@ -18,9 +23,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-        assert_eq!("hello from lib!", greet());
+    fn test_from_input() {
+        assert_eq!(from_input("1"), vec![1]);
+        assert_eq!(from_input("1\n\n2"), vec![1, 2]);
+        assert_eq!(from_input("1\n\n2\n3"), vec![1, 5]);
+    }
+
+    #[test]
+    fn test_most_calories() {
+        assert_eq!(most_calories(vec![1, 5]), 5);
+        assert_eq!(most_calories(vec![1, 5, 8, 3]), 8);
+    }
+
+    #[test]
+    fn test_top3_calories() {
+        assert_eq!(top3_calories(vec![1, 3, 2, 6, 4, 5]), 15); // 4+5+6
     }
 }
--- 2022/rust/src/main.rs
+++ 2022/rust/src/main.rs
@@ -1,6 +1,22 @@
-use aoc::{add, greet};
+use std::fs;
 
+use aoc::{from_input, most_calories, top3_calories};
+
+fn read(day: &str) -> String {
+    return fs::read_to_string(format!("../inputs/{}", day))
+        .expect(&format!("Could not open {} data", day));
+}
+
+/*
+   cargo run
+*/
 pub fn main() {
-    println!("{}", greet());
-    println!("Lib says 1 + 1 = {}", add(1, 1))
+    let input = read("day01");
+    let calories = from_input(&input);
+
+    let answer1 = most_calories(calories.clone());
+    println!("Answer 1: {}", answer1);
+
+    let answer2 = top3_calories(calories);
+    println!("Answer 2: {}", answer2);
 }
