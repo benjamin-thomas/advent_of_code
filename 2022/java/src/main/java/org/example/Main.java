c01fd9456f0b9502182f6235b45d103d54daf15d
Benjamin <benjamin.guy.thomas@gmail.com>
2022-12-06 12:52:18 +0100

    Init Java project

---
+++ 2022/java/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/*
+ * ./gradlew run
+ *
+ * ./gradlew build
+ * java -jar build/libs/aoc-1.0-SNAPSHOT.jar
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
