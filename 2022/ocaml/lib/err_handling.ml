8cb683bbed70dd1e6e416408a366dbd9d7d90d39
Benjamin <benjamin.guy.thomas@gmail.com>
2023-01-14 22:57:17 +0100

    Play with monad operators

--- 2022/ocaml/lib/err_handling.ml
+++ 2022/ocaml/lib/err_handling.ml
@@ -55,9 +55,52 @@ let compute3 (lst : (int, string) Result.t list) =
       | Error e, _ -> Error e)
     ~init:(Ok []) lst
 
+let then_apply2 f ra rb =
+  let open Base in
+  let open Base.Result.Monad_infix in
+  ra >>= fun x -> Result.map ~f:(fun y -> f x y) rb
+
+let compute4 =
+  let open Base in
+  List.fold_right ~f:(then_apply2 List.cons) ~init:(Ok [])
+
+(*
+  >>= is Result.bind
+  >>| is Result.map
+*)
+let then_apply3 f ra rb =
+  let open Base.Result.Monad_infix in
+  ra >>= fun x ->
+  rb >>| fun y -> f x y
+
+(* WOW *)
+let map3 f ra rb rc =
+  let open Base.Result.Monad_infix in
+  ra >>= fun x ->
+  rb >>= fun y ->
+  rc >>| fun z -> f x y z
+
+let compute5 =
+  let open Base in
+  List.fold_right ~f:(then_apply3 List.cons) ~init:(Ok [])
+
 let%test_unit _ =
   expect_result (Ok [ 1; 2; 3 ]) @@ compute3 [ Ok 1; Ok 2; Ok 3 ]
 
 let%test_unit _ =
   expect_result (Error "oops")
   @@ compute3 [ Ok 1; Error "oops"; Error "oops2"; Ok 4 ]
+
+let%test_unit _ =
+  expect_result (Ok [ 1; 2; 3 ]) @@ compute4 [ Ok 1; Ok 2; Ok 3 ]
+
+let%test_unit _ =
+  expect_result (Error "oops")
+  @@ compute4 [ Ok 1; Error "oops"; Error "oops2"; Ok 4 ]
+
+let%test_unit _ =
+  expect_result (Ok [ 1; 2; 3 ]) @@ compute5 [ Ok 1; Ok 2; Ok 3 ]
+
+let%test_unit _ =
+  expect_result (Error "oops")
+  @@ compute5 [ Ok 1; Error "oops"; Error "oops2"; Ok 4 ]
